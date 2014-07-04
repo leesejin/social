ca2ac55d8159eefee38e4c20d8fb5db4c07125bb
leesejin <leesejin@danvincisoftlab.com>
2014-07-04 17:40:58 +0900

    asdsfsdf

--- js/routes.js
+++ js/routes.js
@@ -9,8 +9,9 @@ define([
 	
 		return basicModule.config(function ($routeProvider) {
 
-			//$routeProvider.when('/page1', routeConfig.config('../src/page1.html', 'controllers/page1'));
-			//*%=routeProvider%*//
+			$routeProvider.when('/page1', routeConfig.config('../src/page1.html', 'controllers/page1'));
+			$routeProvider.when('/page2', routeConfig.config('../src/page2.html', 'controllers/page2'));
+
 
 			$routeProvider.otherwise({redirectTo:'/page1'});
 		});
